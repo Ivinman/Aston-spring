43400e7ff6f1fda8c160e49e6669847976171974
Litheraa <Litheraa@gmail.com>
2025-05-03 21:51:49 +0700

    add test for Mapper.toDtoList

--- src/test/java/ru/aston/module4/MapperTest.java
+++ src/test/java/ru/aston/module4/MapperTest.java
@@ -7,7 +7,12 @@ import ru.aston.module4.mapper.UserMapper;
 import ru.aston.module4.mapper.UserMapperImpl;
 import ru.aston.module4.entity.User;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class MapperTest {
 	private final UserMapper mapper = new UserMapperImpl();
@@ -38,16 +43,36 @@ public class MapperTest {
 	}
 
 	@Test
-    void testUpdateUserFromDto() {
-        User user = new User(42L, "Spy", 50, "mail@mail.com");
+	void updateUserFromDto_test() {
+		User user = new User(42L, "Spy", 50, "mail@mail.com");
 
-        UserModel userModel = new UserModel(74L, "Traitor", 30, "newmail@mail.com");
+		UserModel userModel = new UserModel(74L, "Traitor", 30, "newmail@mail.com");
 
-        mapper.updateUserFromDto(userModel, user);
+		mapper.updateUserFromDto(userModel, user);
 
-        assertEquals(42L, user.getId());
-        assertEquals("Traitor", user.getName());
-        assertEquals(30, user.getAge());
-        assertEquals("newmail@mail.com", user.getEmail());
-    }
+		assertAll(
+				() -> assertEquals(42L, user.getId()),
+				() -> assertEquals("Traitor", user.getName()),
+				() -> assertEquals(30, user.getAge()),
+				() -> assertEquals("newmail@mail.com", user.getEmail())
+		);
+	}
+
+	@Test
+	void mapToDtoList_test() {
+		List<User> userList = List.of(
+				new User(42L, "Spy", 50, "mail@mail.com"),
+				new User(74L, "Dog", 10, "mail@mail.ru")
+		);
+
+		List<UserDto> userDtoList = mapper.toDtoList(userList);
+
+		assertThat(userDtoList)
+				.usingRecursiveFieldByFieldElementComparator()
+				.containsExactlyInAnyOrderElementsOf(
+						userList.stream()
+								.map(user -> new UserDto(user.getName(), user.getAge(), user.getEmail()))
+								.toList()
+				);
+	}
 }
