e9b445d633369ac76f7d7d04f4a7cce8a6491652
Litheraa <Litheraa@gmail.com>
2025-05-08 12:36:50 +0700

    fix resieved notices, add java-doc for UserServiceImpl

--- src/main/java/ru/aston/module4/controller/UserController.java
+++ src/main/java/ru/aston/module4/controller/UserController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +22,6 @@ import ru.aston.module4.service.UserService;
 
 import java.util.List;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
